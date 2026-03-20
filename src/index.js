21c8ad7355c710abb9f7ed451560f4ecc9d7d4c4
jayy-codes07 <hadiyajay2010@gmail.com>
2026-03-20 16:22:02 +0530

    create utils for proper environment

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./.env" });
 console.log("ENV:", process.env.MONGO_URL);
 
-connectDB();
+connectDB()
+  .then(() => {
+    app
+      .listen(process.env.PORT, () => {
+        console.log(`localhost port : ${process.env.PORT}`);
+      })
+      .on("error", (error) => {
+        console.log("there is error => ", error);
+        throw Error;
+      });
+  })
+  .catch((err) => {
+    console.error("there is error".err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = (fn) => async () => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({ success: false, message: err.message });
+  }
+};
