027f849d903e51eece42f526fcf60e075237d6dc
jayy-codes07 <hadiyajay2010@gmail.com>
2026-03-20 12:48:49 +0530

    connect to DB compass

---
+++ src/db/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectioninstance = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
+    console.log(`database is connected successfully  => ${connectioninstance.connection.host}`);
+    
+  } catch (error) {
+    console.error("there is error in DB connection", error);
+  }
+};
+
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("backend start");
+// require("env").config()
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import connectDB from "./db/db.js";
+
+dotenv.config({ path: "./.env" });
+console.log("ENV:", process.env.MONGO_URL);
+
+connectDB();
