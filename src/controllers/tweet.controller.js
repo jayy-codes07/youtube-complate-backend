082957d744e71fec4c97bb6b6e744bed8277ddfd
jayy-codes07 <hadiyajay2010@gmail.com>
2026-04-09 23:59:15 +0530

    implement createTweet function with content validation and response handling

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,13 +1,29 @@
 import mongoose, { isValidObjectId } from "mongoose"
-import {Tweet} from "../models/tweet.model.js"
-import {User} from "../models/user.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Tweet } from "../models/tweet.model.js"
+import { User } from "../models/user.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const { content } = req.body;
+
+    if (!content?.trim()) {
+        throw new ApiError(400, "there is no content available there ")
+    }
+
+    const tweet = await Tweet.create({ owner: req.user._id, content: content?.trim() })
+
+    if (!tweet) {
+        throw new ApiError(500, "there is problem in creating tweet")
+    }
+
+    res.status(200).json(new ApiResponse(200, tweet, "tweet created successfully"))
+
 })
+
+
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
